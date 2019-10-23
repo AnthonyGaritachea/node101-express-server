0fc5dffd6d9df1b095bd013cfe5f757b9a455f32
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-23 13:04:56 -0700

    project done

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const morgan = require('morgan')
 
+const topSpotsData = require('./data.json');
+
 const app = express();
 
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.status(200).send('hello world');
+});
+
+app.get('/data', (req, res) => {
+    res.send(topSpotsData);
 });
 
 module.exports = app;
